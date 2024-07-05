fe2c195da24998d8decdf0e2fdde50f99d8723ca
AdrianAlmario <adrianjames.almario123@gmail.com>
2024-07-05 20:45:19 +0800

    first commit

---
+++ assets/script/index.js
@@ -0,0 +1,27 @@
+/*const body = document.body
+const div = document.createElement("div")
+body.append(div)
+
+const strong = document.createElement("strong")
+strong.innerText = "Hello World"
+div.append(strong)*/
+
+const div = document.querySelector("div")
+const spanHi = document.querySelector("#hi")
+const spanBye = document.querySelector("#bye")
+
+//spanHi.classList.add("new-class")
+//spanHi.classList.remove("hi1")
+
+//spanHi.style.color = "red"
+//spanHi.style.backgroundColor = "red"
+//spanBye.remove()
+//div.append(spanBye)
+
+//console.log(spanHi.getAttribute("id"))
+//spanHi.setAttribute("id", "change-id")
+//spanHi.removeAttribute("id")
+
+//console.log(spanHi.dataset)
+
+
